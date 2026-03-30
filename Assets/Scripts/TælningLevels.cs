6ad24861187bee66efe7aeba47fcf02655886b91
Sebastian <sebastianleosorensen@gmail.com>
2026-03-30 14:30:41 +0200

    oh my boolean

--- Assets/Scripts/TælningLevels.cs
+++ Assets/Scripts/TælningLevels.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class TælningLevels : MonoBehaviour
 {
@@ -8,11 +9,49 @@ public class TælningLevels : MonoBehaviour
 
     public List<int> numbers = new List<int>{1,2,3,4,5};
 
+    public List<int> answerIndex = new List<int>{0,0,0,0,0};
+
     public int numberToGuess;
+    
+    public int randomAnswer;
+
+    public int correctAnswerIndex;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (var x in answerIndex) {
+        Debug.Log( x.ToString());
+        }
+        numberToGuess = Random.Range(1, numbers.Count+1);
+        Debug.Log(numberToGuess);
+        correctAnswerIndex = Random.Range(0, answerIndex.Count);
+        Debug.Log(correctAnswerIndex);
+        for (int i = 0; i < answerIndex.Count; i++)
+        {            
+            if (i == correctAnswerIndex)
+            {
+                Debug.Log("Setting the correct number in this index");
+                answerIndex[i] = numberToGuess;
+                Debug.Log(numberToGuess);
+            }
+            else
+            {
+                bool tryingOriginalNumber = true;
+                while (tryingOriginalNumber)
+                {
+                    randomAnswer = Random.Range(0,8);
+                    if (!answerIndex.Contains(randomAnswer) && randomAnswer != numberToGuess)
+                    {
+                        Debug.Log("Found random Original number");
+                        answerIndex[i] = randomAnswer;
+                        Debug.Log(randomAnswer);
+                        tryingOriginalNumber = false;
+                    }
+                }
+            }
+        }
     }
 
     public void NextQuestion()
