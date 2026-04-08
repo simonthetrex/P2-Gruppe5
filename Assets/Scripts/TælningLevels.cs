80d36664a48f860c02dcab6d86df8495e21a8784
Sebastian <sebastianleosorensen@gmail.com>
2026-04-08 14:28:32 +0200

    buttons done and done.

--- Assets/Scripts/TælningLevels.cs
+++ Assets/Scripts/TælningLevels.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
+using TMPro;
+using UnityEngine.UI;
 
 public class TælningLevels : MonoBehaviour
 {
@@ -11,57 +14,86 @@ public class TælningLevels : MonoBehaviour
 
     public List<int> answerIndex = new List<int>{0,0,0,0,0};
 
+    public Button[] buttons;
+
     public int numberToGuess;
     
     public int randomAnswer;
 
     public int correctAnswerIndex;
-
+    
+    private Button lastClickedButton;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        foreach (var x in answerIndex) {
-        Debug.Log( x.ToString());
+        GameObject buttonsParent = GameObject.Find("buttons");
+        if (buttonsParent != null)
+        {
+            buttons = buttonsParent.GetComponentsInChildren<Button>();
         }
+        NextQuestion();
+    }
+
+    public void NextQuestion()
+    {
+        if (lastClickedButton != null)
+        {
+            lastClickedButton.GetComponent<Image>().color = Color.white;
+        }
+
         numberToGuess = Random.Range(1, numbers.Count+1);
-        Debug.Log(numberToGuess);
         correctAnswerIndex = Random.Range(0, answerIndex.Count);
-        Debug.Log(correctAnswerIndex);
         for (int i = 0; i < answerIndex.Count; i++)
-        {            
+        {
             if (i == correctAnswerIndex)
             {
-                Debug.Log("Setting the correct number in this index");
                 answerIndex[i] = numberToGuess;
-                Debug.Log(numberToGuess);
             }
             else
             {
                 bool tryingOriginalNumber = true;
                 while (tryingOriginalNumber)
                 {
-                    randomAnswer = Random.Range(0,8);
+                    randomAnswer = Random.Range(0, 8);
                     if (!answerIndex.Contains(randomAnswer) && randomAnswer != numberToGuess)
                     {
-                        Debug.Log("Found random Original number");
                         answerIndex[i] = randomAnswer;
-                        Debug.Log(randomAnswer);
                         tryingOriginalNumber = false;
                     }
                 }
             }
         }
+        for (int x = 0; x < buttons.Length; x++)
+        {
+            TextMeshProUGUI buttonTextComponent = buttons[x].GetComponentInChildren<TextMeshProUGUI>();
+            buttonTextComponent.text = answerIndex[x].ToString();
+        }
+        numbers.Remove(numberToGuess);
     }
 
-    public void NextQuestion()
+    public void CheckAnswer(Button clickedButton)
     {
-        numberToGuess = Random.Range(1, numbers.Count+1);
-        numbers.RemoveAt(numberToGuess);
-        if (answered == true)
+        lastClickedButton = clickedButton;
+        TextMeshProUGUI clickedText = clickedButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (clickedText.text == numberToGuess.ToString())
         {
-            
+            clickedButton.GetComponent<Image>().color = Color.green;
+            StartCoroutine(animate(3.0f));
         }
+        else
+        {
+            clickedButton.GetComponent<Image>().color = Color.red;
+            StartCoroutine(animate(3.0f));
+        }
+    }
+
+    private IEnumerator animate(float time)
+    {
+        numbers.Insert(numberToGuess-1, numberToGuess);
+        yield return new WaitForSeconds(time);
+        NextQuestion();
     }
     // Update is called once per frame
     void Update()
